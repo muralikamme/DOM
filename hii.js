5a12068dd5e9c63fabcfa57a38cbf9ae7899de75
Prashanth yejje <pyejje@bitbucket.allmysons.com>
2024-09-14 21:35:20 +0530

    DocumentObjectMobel

---
+++ hii.js
@@ -0,0 +1,42 @@
+let a=[
+    {
+        Name:"murali",
+        age:23
+
+    },
+    {
+       Name:"hari" ,
+       age:24
+    },
+    {
+        Name:"murali",
+        age:23
+
+    },
+    {
+       Name:"hari" ,
+       age:24
+    },
+    {
+        Name:"murali",
+        age:23
+
+    },
+    {
+       Name:"hari" ,
+       age:24
+    },
+    {
+        Name:"murali",
+        age:23
+
+    },
+    {
+       Name:"hari" ,
+       age:24
+    }
+]
+
+for(var h in a){
+    console.log(a[h]["Name"])
+}
